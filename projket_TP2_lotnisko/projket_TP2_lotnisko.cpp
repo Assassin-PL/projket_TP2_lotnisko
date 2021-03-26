bcc058792a9d3ebc4e162af7d6b259fab311fed0
Michał Zarzycki <s184856@o365.student.pg.edu.pl>
2021-03-26 20:00:33 +0100

    dodalem klase samolotu i tam bedziemy listy tych samolotow robili, i logika zderzen i tego wszystkiego to bedzie na tej liscie samolotow, a do plansze bedziemy zmieniac samolotami jakby co

--- projket_TP2_lotnisko/projket_TP2_lotnisko.cpp
+++ projket_TP2_lotnisko/projket_TP2_lotnisko.cpp
@@ -1,43 +1,80 @@
 ﻿#include <iostream>
 #include <random>
-#include <vector>
+#include <list>
+#include <array>
+#include <string>
 
 using namespace std;
 
-class plansza
+const int ROW = 10;
+const int COL = 12;
+
+class Tile
 {
 public:
-	bool is_visited = 0;
-	int x, y;
-	plansza();
-	~plansza();
-	void visit_point()
+	char tile_view = ' ';
+	Tile(char new_tile);
+	~Tile();
+	void print_tile()
 	{
-		is_visited = 1;
+		cout << tile_view;
+	}
+	void set_tile(char tile_new)
+	{
+		tile_view = tile_new;
 	}
 private:
 
 };
 
-plansza::plansza()
+Tile::Tile(char new_tile = ' ')
 {
-	x = 0;
-	y = 0;
-	is_visited = 0;
+	tile_view = new_tile;
 }
 
-plansza::~plansza()
+Tile::~Tile()
 {
 }
+
+class Plane
+{
+public:
+	int x, y, deley, command;//czy dodac stringa nazwe?
+	bool direction;
+	char nazwa;
+	Plane(int a, int b, int c, int d, bool e, char f);
+	~Plane();
+
+private:
+
+};
+
+Plane::Plane(int a, int b, int c, int d, bool e, char f)
+{
+	x = a;
+	y = b;
+	deley = c;
+	command = d;
+	direction = e;
+	nazwa = f;
+}
+
+Plane::~Plane()
+{
+}
+
+void set_board(array<array<Tile, COL>, ROW> &board);
+void view_board(array<array<Tile, COL>, ROW>& board);
 
 int main()
 {
+	array<array<Tile, COL>, ROW> board;
     cout << "Hello World!\n";
-	plansza* ptr_plansza = new plansza[1];
-	cout << ptr_plansza->is_visited<<endl;
-	ptr_plansza->visit_point();
-	cout << ptr_plansza->is_visited;
-	delete[] ptr_plansza;
+	set_board(board);
+	view_board(board);
+	list<Plane> samolot;
+	Plane obj1(1,2,3,3,0,'A');
+	samolot.push_back(obj1);
     return 0;
 }
 
@@ -49,3 +86,48 @@ int algorytm_losujacy(int beg, int end)
 
 	return distr(gen);
 }
+
+void set_board(array<array<Tile, COL>, ROW> &board)
+{
+	char board_borders = '=';
+	char board_rows = '|';
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COL; j++)
+		{
+			if (i == 0 || i == (ROW - 1))
+			{
+				board[i][j].set_tile(board_borders);
+			}
+			else
+			{
+				if (j == 1 || j == (COL - 2))
+				{
+					board[i][j].set_tile(board_rows);
+				}
+			}
+
+		}
+	}
+}
+
+void view_board(array<array<Tile, COL>, ROW>& board)
+{
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COL; j++)
+		{
+			board[i][j].print_tile();
+		}
+		cout << endl;
+	}
+}
+//zastosowac clean coda zeby bylo ok
+//dodajemy samoloty na koncu(push_back), a usuwamy na poczatku(pop_front)
+//tworzymy nieskoczona petle programu
+//1 losujemy czy ma sie pojawic nowy samolot, globalna zmienna probalistyczna int (od 0 do 100) losujemy w randomie liczbe od 0 do 100 i jesli wylosowana jest mniejsza od prob to losujemy samolot
+//2 losowanie czy ma sie samolot poruszac w lewo czy w prawo
+//3 loswanie na ktorym torze ma sie pojawic i zrobic if czy nie bedzie kolizji przy pojawieniu sie nowego samolotu i losowac az znajdzi sie miejsce w odleglosci 7 pol
+//4 logika zderzen dziala do 2 pol odleglosci, przelatuje forem po kazdym rzedzie i sprawdzam czy istnije taka sytuacja ze 
+//5 dodac do klasy atrybut oznaczajcy ile razy za wykonac dany rozkaz
+//6zrobic logike jak doleci
