0f0d4ff2623d231c3ad41a3fb9403e1e7911c4db
Pocwiardo <80002864+Pocwiardo@users.noreply.github.com>
2021-04-03 12:36:16 +0200

    s

--- projket_TP2_lotnisko/projket_TP2_lotnisko.cpp
+++ projket_TP2_lotnisko/projket_TP2_lotnisko.cpp
@@ -40,7 +40,7 @@ Tile::~Tile()
 class Plane
 {
 public:
-	int x, y, deley=0, command;
+	int x, y, deley, command;
 	bool direction;
 	char nazwa;
 	Plane(int row_value, int column_value, int plain_value_of_deley, int set_command, bool flight_direction, char name_of_plain);
@@ -131,6 +131,7 @@ int main()
 	setlocale(LC_CTYPE, "Polish");
 	array<array<Tile, COL>, ROW> board;
 	set_board(board);
+	//view_board(board);
 	list<Plane> samolot;
 	losuj_samolot(samolot);
 	fill_the_board(board, samolot);
